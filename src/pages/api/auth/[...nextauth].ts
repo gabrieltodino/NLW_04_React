02592a843b0141cb6e869684444c25db9d13506a
Gabriel Todino <gabrieltodino08@gmail.com>
2021-03-31 16:42:20 -0300

    test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import Providers from 'next-auth/providers'
 const options = {
     providers: [
         Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientId: e372a68989302b87f106,
+            clientSecret: c0880bf964d9a9b6531a7b035d948874c87471c2
         }),
     ],
 }
